3f3862e109846f9d856962b5082178a24dac5633
emon84090 <emon84090@gmail.com>
2023-10-03 23:50:09 +0600

    done

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbCOnnect = async () => {
-
     try {
         const data = await mongoose.connect(`mongodb+srv://${process.env.USERNAMEDTI}:${process.env.PASSWORD}@cluster0.dw47wnx.mongodb.net/dtiblood`);
         console.log(`database conected`);
